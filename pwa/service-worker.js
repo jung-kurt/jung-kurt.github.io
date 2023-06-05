3a4ae383609a0214d1f02f790e08f8485cbff253
Kurt <kurt.w.jung@gmail.com>
2023-06-05 11:33:56 -0400

    Revise grammar

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,5 +1,5 @@
 // version gets updated for every release
-const version = '2023-06-05:15:15:32';
+const version = '2023-06-05:15:29:24';
 
 // array of assets that will be cached on client
 const cacheAssets = [ '/pwa/pwa.png', '/pwa/favicon.ico', '/pwa/index.css', '/pwa/', '/pwa/index.js', '/pwa/utility.js' ];
